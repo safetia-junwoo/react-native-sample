abdca4ae73983950c869fe5c118a41bd035209ac
DESKTOP-AASJ58P\admin <tigerbaram@gmail.com>
2023-05-17 16:45:12 +0900

    'splash-screen'

--- android/app/src/main/java/com/dinga/MainActivity.java
+++ android/app/src/main/java/com/dinga/MainActivity.java
@@ -4,8 +4,11 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -14,6 +17,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "dinga";
+  }
+
+  @Override 
+  protected void onCreate(Bundle savedInstanceState) { 
+      SplashScreen.show(this);
+     super.onCreate(null);      
   }
 
   /**
